59a21110aa24c9401330676826dd04251d5296fb
Sam <sam@tealstreet.io>
2021-05-14 13:26:11 -0700

    scaled orders working [redo]

--- trade.py
+++ trade.py
@@ -1,60 +1,70 @@
-import os
 import sys
 import ccxt
-
+import os
+from pprint import pprint
 
 # replace with your key / secret
 key =  os.environ.get('ftx_key')
 secret =  os.environ.get('ftx_secret')
 
-
 exchange = ccxt.ftx({
     'apiKey': key,
     'secret': secret
 })
+exchange.load_markets()
+
+
+# cancel all open orders for symbol
+def cancel_all(symbol):
+    res = exchange.cancel_all_orders(symbol)
+    pprint(res)
+
 
 # create and execute a scaled order
-def scaled_order(symbol, side, total_str, start_price_str, end_price_str, num_orders_str):
-    # convert cl args to numbers
-    total = float(total_str)
-    start_price = float(start_price_str)
-    end_price = float(end_price_str)
-    num_orders = int(num_orders_str)
+def scaled_order(symbol, side, total, start_price, end_price, num_orders):
+    # cast command line args to proper types for math
+    total = float(total)
+    start_price = float(start_price)
+    end_price = float(end_price)
+    num_orders = int(num_orders)
 
-    # params for order
+    # params for the order
     params = {'postOnly': True}
 
     # size of each order
-    order_amount = total/num_orders
+    order_amount = total / num_orders
 
-    # step size of each order price starting at start_price
-    order_inc = (end_price-start_price)/(num_orders - 1)
+    # step size of each order price starting from start_price
+    step_size = (end_price-start_price)/(num_orders-1)
 
     # loop to generate orders
     for order_num in range(num_orders):
-        # this order price using step size starting from start_price
-        order_price = round(start_price + order_inc*order_num, 0)
+        # the price of this order, using step_size to calculate offset
+        order_price = start_price + step_size*order_num
         # send the order
-        exchange.create_order(symbol, "limit", side, order_amount, order_price, params)
+        res = exchange.create_order(symbol, "limit", side, order_amount, order_price, params)
+        pprint(res)
 
 
-
-
-# create a simple order
-def simple_order(symbol, limit, side,  amount, price):
+# create and execute a basic order
+def simple_order(symbol, side, amount, price):
+    # params for the order
     params = {'postOnly': True}
-    exchange.create_order(symbol, "limit", side, amount, price)
-
+    # send the order
+    res = exchange.create_order(symbol, "limit", side, amount, price, params)
+    pprint(res)
 
 
 # parse function name and args and call function
 def main():
-    # name of function to call
+    # name of function
     func_name = sys.argv[1]
-    # args passed directly to function
+    # args passed to function
     args = sys.argv[2:]
-    # call the function
+    # calling the function
     globals()[func_name](*args)
+
+
 
 
 if __name__ == "__main__":
